dcc6aa7bf6dfeba773bc95c3263b6e15883ed970
Alexander Santos <lekxandy@hotmail.com>
2021-08-23 15:13:57 -0300

    criado o serviço de envio de email com nodemailer e gmail

---
+++ README.md
@@ -0,0 +1,7 @@
+# Envio de emails com nodeJS
+
+- Uso da lib nodemailer e recursos do SMTP do gmail
+- Uso do fs do node para ler arquivos e pastas
+- Lê uma lista de contatos no formato csv (lista_emails.csv)
+- Lê a pasta de attachments e pega o nome e o path dos possíveis anexos, anexando-os na mensagem
+- A mensagem principal escrita no arquivo message.txt, lida altera dinamicamente a variável ${nome} pelo nome do contato
---
+++ index.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const nodemailer = require('nodemailer');
+require('dotenv').config()
+
+const lista_csv = fs.readFileSync(__dirname + '/lista_emails.csv', 'utf-8')
+
+const lista = lista_csv.split(/\r?\n|\r/g)
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.USER,
+        pass: process.env.PASS
+    }
+})
+
+const attachments_list = fs.readdirSync(__dirname + '/attachments')
+
+const attachments = attachments_list.map(a => {
+    return {
+        filename: a,
+        path: `./attachments/${a}`
+    }
+})
+
+for (contato of lista) {
+
+    let [nome, email] = contato.split(',')
+    let subject = 'Email de teste'
+    let text = fs.readFileSync(__dirname + '/message.txt', 'utf-8').replace('${nome}', nome)
+
+    const mailOptions = {
+        from: process.env.USER,
+        to: `${email}`,
+        subject,
+        text,
+        attachments
+
+    }
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(`Email enviado com sucesso para ${nome} `)
+        }
+    })
+
+}
+
